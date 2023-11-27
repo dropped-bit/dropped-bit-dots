a152f871b93222ae4474a7ce204ae8d4df7ee79e
Martin Holmes <m.holmes.de@gmail.com>
2023-11-27 08:55:29 +0100

    improved nvconf alias to not show nvimtree on startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ starship init fish | source
 alias ls='exa --color=always --icons --long'
 alias ll='exa --color=always --icons --long --all'
 alias lt='exa --color=always --icons --long --all --tree --level=3'
-alias nvconf="nvim $HOME/.config/nvim/"
+alias nvconf="cd $HOME/.config/nvim && nvim"
 alias python="python3"
 # alias neofetch='neofetch --config ~/Builds/neofetch-themes/small/ozozfetch2.conf'
 alias grep='grep --color=auto'
