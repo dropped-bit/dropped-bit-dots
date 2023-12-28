dd3153ad5e5ebbc818398f1376cb7a7332eb37dc
Martin Holmes <m.holmes.de@gmail.com>
2023-12-28 20:07:11 +0100

    alacritty to toml + wallpaper organization

--- .config/dropped-distros/install.sh
+++ .config/dropped-distros/install.sh
@@ -1,17 +1,22 @@
 # GRAPHIC DRIVERS
-sudo pacman -S nvidia-dkms mesa intel-ucode
+sudo pacman -S nvidia-dkms mesa intel-ucode nvidia-utils
 yay -S envycontrol
 yay -S libva-vdpau-driver-vp9-git # might help with chrome launches https://wiki.archlinux.org/title/chromium
 
 # GENERAL PACAKGES PACMAN
 sudo pacman -S make btop alacritty eza fish starship font-manager gparted polkit-gnome gnome-keyring thunar wofi python-pip python-pywal grim slurp wl-clipboard cliphist mako libnotify network-manager-applet brightnessctl jq python3 fzf ripgrep nodejs npm unzip swayidle flatpak
 
+# THUNAR
+pacman -S thunar-volman tumbler gvfs
+
 # AUDIO
 pacman -S pipewire wireplumber pipewire-alsa pipewire-pulse helvum pamixer
 
 # BLUETOOTH
-
 pacman -S bluez bluez-utils blueman
+
+# PRINTER
+pacman -S cups
 
 # GENERAL PACKAGES YAY
 yay -S waybar-hyprland swww-git swaylock-effects wlogout nwg-look adw-gtk3-git
@@ -28,6 +33,7 @@ yay -S tela-circle-icon-theme-git volantes-cursors flat-remix-gtk
 
 systemctl enable bluetooth
 systemctl enable NetworkManager
+systemctl enable cups
 
 # LOW PRIORITY ITEMS
 
