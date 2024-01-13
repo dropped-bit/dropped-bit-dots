399cc6b7daa9e0a3ca90bd32375b8eb7afaffa00
Martin Holmes <m.holmes.de@gmail.com>
2024-01-13 13:47:07 +0100

    random changes

--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 # Should be improved alot!
 
-wallpaper=~/.config/wallpapers/catppuccin/6.png
+if [ $# -eq 0 ]; then
+    echo "Script requires wallpaper path as arguement '.wallpaper.sh $HOME/.config/wallpapers/catppuccin/1.png'"
+    exit 1
+fi
+
+wallpaper=$1
 
 swww init
 
@@ -13,6 +18,7 @@ then
     swww img $wallpaper --transition-step 1 --transition-fps 60 --transition-type random
     cp $wallpaper $HOME/.cache/current_wallpaper.png
     waybar &>/dev/null & disown;
+    echo "If you want to update grub image run 'sudo grub-mkconfig -o /boot/grub/grub.cfg'"
 else
     echo "wallpaper is not available"
     echo "please install"
