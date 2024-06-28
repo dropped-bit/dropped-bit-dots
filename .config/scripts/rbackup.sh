fe72f7a5e3d06ca3a4252e39bf68ae35354eaa64
Martin Holmes <m.holmes.de@gmail.com>
2024-06-28 12:14:19 +0200

    rbackup.sh simplifacation

--- .config/scripts/rbackup.sh
+++ .config/scripts/rbackup.sh
@@ -3,4 +3,13 @@
 DATE=$(date '+%F')
 DISTRO=$(awk -F= '$1=="ID" { print $2 }' /etc/os-release | grep -v '^$')
 
-rsync -avz --progress /home/holmes/ /run/media/holmes/HolmesSSD/Fedora\ Vault/"$DISTRO"_"$DATE"_1/
+DESTINATION="/run/media/holmes/Ventoy/BackUp/fedora_240629"
+# rsync -avjz --progress /home/holmes/ /run/media/holmes/HolmesSSD/Fedora\ Vault/"$DISTRO"_"$DATE"_1/
+rsync -avz --progress $HOME/.config $DESTINATION
+rsync -avz --progress $HOME/Projects $DESTINATION
+
+rsync -avz --progress $HOME/dropped-bit-dots $DESTINATION
+rsync -avz --progress $HOME/.ssh $DESTINATION
+rsync -avz --progress $HOME/.gitconfig $DESTINATION
+rsync -avz --progress $HOME/.zsh_history $DESTINATION
+
